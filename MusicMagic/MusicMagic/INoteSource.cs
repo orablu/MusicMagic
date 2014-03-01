f6ff71ebd54320198848936a2d259a798b4c6384
Wes Rupert <ora@outlook.com>
2014-02-28 23:46:06 -0500

    Implement Note.cs

---
+++ MusicMagic/MusicMagic/INoteSource.cs
@@ -0,0 +1,14 @@
+﻿using SharpDX.Multimedia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMagic {
+    interface INoteSource {
+        string Path { get; set; }
+        int LoopBegin { get; set; }
+        int LoopLength { get; set; }
+    }
+}
---
+++ MusicMagic/MusicMagic/Note.cs
@@ -0,0 +1,85 @@
+﻿using SharpDX.Multimedia;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.XAudio2;
+using Windows.Storage;
+
+namespace MusicMagic {
+    class Note : INote {
+        public List<INoteSource> Sources {
+            get;
+            set;
+        }
+
+        public XAudio2 Device { get; set; }
+
+        public long Start {
+            get;
+            set;
+        }
+
+        private int _length;
+        public int Length {
+            get {
+                return _length;
+            }
+            set {
+                _length = value;
+                destroySourceVoice();
+            }
+        }
+
+        private int _pitch;
+        public int Pitch {
+            get {
+                return _pitch;
+            }
+            set {
+                _pitch = value;
+                destroySourceVoice();
+            }
+        }
+
+
+        private SourceVoice _sourceVoice;
+        private AudioBuffer _buffer;
+        public SourceVoice SourceVoice {
+            get {
+                if (_sourceVoice == null) {
+                    _sourceVoice = CreateSourceVoiceFromSource(Sources[Pitch]);
+                }
+                return _sourceVoice;
+            }
+        }
+
+        private SourceVoice CreateSourceVoiceFromSource(INoteSource source) {
+            var localStorage = ApplicationData.Current.LocalFolder;
+            var file = localStorage.OpenStreamForReadAsync(source.Path).Result;
+            var stream = new SoundStream(file);
+            var format = stream.Format;
+            _buffer = new AudioBuffer {
+                Stream = stream.ToDataStream(),
+                AudioBytes = (int)stream.Length,
+                Flags = BufferFlags.EndOfStream,
+                LoopBegin = source.LoopBegin,
+                LoopLength = source.LoopLength,
+                LoopCount = Length
+            };
+            stream.Dispose();
+            return new SourceVoice(Device, format, true);
+        }
+
+        private void destroySourceVoice() {
+            if (_sourceVoice != null) {
+                _sourceVoice.DestroyVoice();
+                _buffer.Stream.Dispose();
+                _sourceVoice.Dispose();
+            }
+            _sourceVoice = null;
+        }
+    }
+}
