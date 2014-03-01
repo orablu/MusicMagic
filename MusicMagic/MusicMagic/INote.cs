77446203f7cadff90107d6294a374a6dba862c8d
Wes Rupert <ora@outlook.com>
2014-02-28 21:34:00 -0500

    Add note/note stream interfaces

---
+++ MusicMagic/MusicMagic/INote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.XAudio2;
+using SharpDX.Multimedia;
+
+namespace MusicMagic {
+    interface INote {
+        double Start { get; set; }
+        double Length { get; set; }
+        int Pitch { get; set; }
+        SoundStream Stream { get; }
+    }
+}
---
+++ MusicMagic/MusicMagic/INoteStream.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMagic {
+    interface INoteStream : IEnumerable<INote> {
+        IEnumerable<INote> Notes { get; set; }
+        IEnumerable<INote> NotesInRange(TimeSpan start, TimeSpan end);
+        IEnumerator<INote> GetEnumerator();
+    }
+}
